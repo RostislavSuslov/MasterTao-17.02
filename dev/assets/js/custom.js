143536b7e8bfd0ba5bc004e2982d378610c84e48
RostSuslov <suslovros@gmail.com>
2023-05-26 21:27:16 +0300

    template titles

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -7,7 +7,8 @@ $( document ).ready(function() {
             {
               breakpoint: 991,
               settings: {
-                arrows: false
+                arrows: false,
+                // fade: true,
               }
             },
              
