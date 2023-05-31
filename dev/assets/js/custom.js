2411fa19a50cef48220f3928eb812699b6491027
RostSuslov <suslovros@gmail.com>
2023-05-31 11:23:59 +0300

    creat form block

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,24 +1,39 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     $('.first-screen-slider').slick({
         dots: true,
         speed: 1000,
         appendArrows: ".container-arrows",
-        responsive: [
-            {
-              breakpoint: 991,
-              settings: {
-                arrows: false,
-                // fade: true,
-              }
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+                    // fade: true,
+                }
             },
-             
+
         ]
     });
 
     // $('.navbar-toggler').on('click', function(){
     //   $('.navbar').toggleClass('margin-bottom')
     // })
- 
+
 
 });
+
+
+function checkboxContainerPadding() {
+    const checkButton2 = document.querySelectorAll('.check-button');
+    
+    checkButton2.forEach(item => {
+        item.closest('[class*="col"]').classList.add('padding-top');
+    }) 
+}
+
+
+if (document.querySelector('.calculation-form')) {
+      checkboxContainerPadding()
+} else {
+    null
+}
 
